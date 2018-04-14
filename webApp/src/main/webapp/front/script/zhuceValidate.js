51c1e3807ba0fcdf20b5f786ddbb84abbe15ee15
manguogo <manguogo@aliyun.com>
2018-04-14 22:09:37 +0800

    Revert "base2"
    This reverts commit 51aeff5

--- webApp/src/main/webapp/front/script/zhuceValidate.js
+++ webApp/src/main/webapp/front/script/zhuceValidate.js
@@ -16,5 +16,31 @@ function passwordV() {
         document.getElementById("passwordSpan").innerText = "";
 }
 
+function password2V() {
+    var p = $("#password").val();
+    var p2 = $("#password2").val();
+    if(p == "")
+        document.getElementById("password2Span").innerText = "请验证密码";
+    else if(p != p2)
+        document.getElementById("password2Span").innerText = "密码确认有误";
+    else
+        document.getElementById("password2Span").innerText = "";
+}
 
+function phoneV() {
+    var ph = $("#phone").val();
+    if(ph == "")
+        document.getElementById("phoneSpan").innerText = "请输入电话号码";
 
+    else
+        document.getElementById("phoneSpan").innerText = "";
+}
+
+function addrV() {
+    var addr = $("#addr").val();
+    if(addr == "")
+        document.getElementById("addrSpan").innerText = "请输入配送地址";
+    else
+        document.getElementById("addrSpan").innerText = "";
+
+}
