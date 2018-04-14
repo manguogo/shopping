51aeff54b01c91cfe54ddcab5fcef9e296098f11
manguogo <manguogo@aliyun.com>
2018-04-14 21:35:30 +0800

    base2

--- webApp/src/main/webapp/front/script/zhuceValidate.js
+++ webApp/src/main/webapp/front/script/zhuceValidate.js
@@ -1,5 +1,6 @@
-$(document).ready(
-    $(":input[id='username']").click(function () {
-        alert("ok")
-    })
-)
+function usernameV() {
+    var u = $("#username").val();
+    if(u == "")
+        document.getElementById("usernameSpan").innerText = "用户名不能为空";
+
+}
