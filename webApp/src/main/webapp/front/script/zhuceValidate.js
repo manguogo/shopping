5605ae9dcad4196c16b0bb7d04e11a1e90b46a76
manguogo <manguogo@aliyun.com>
2018-04-14 21:56:45 +0800

    Revert "base2"
    This reverts commit 51aeff5

--- webApp/src/main/webapp/front/script/zhuceValidate.js
+++ webApp/src/main/webapp/front/script/zhuceValidate.js
@@ -1,6 +1,24 @@
+
 function usernameV() {
     var u = $("#username").val();
     if(u == "")
         document.getElementById("usernameSpan").innerText = "用户名不能为空";
+    else
+        document.getElementById("usernameSpan").innerText = "";
 
 }
+
+function passwordV() {
+    var p = $("#password").val();
+    if(p == "")
+        document.getElementById("passwordSpan").innerText = "密码不能为空";
+    else
+        document.getElementById("passwordSpan").innerText = "";
+}
+
+$(document).ready(
+    $(":input[id='username']").click(function () {
+        alert("ok")
+    })
+)
+
